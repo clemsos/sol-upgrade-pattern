c2d759fcc54475c45b1129361ae5d79cc231d2b0
Clement Renaud <clement@clementrenaud.com>
2021-11-09 11:07:07 +0100

    clone and init proxy deploy

--- contracts/Deployer.sol
+++ contracts/Deployer.sol
@@ -2,8 +2,10 @@
 pragma solidity ^0.8.0;
 
 import './IBox.sol';
+import './Box.sol';
 import './utils/Clone2Factory.sol';
-
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 
 contract Deployer {
 
@@ -20,17 +22,49 @@ contract Deployer {
     return keccak256(abi.encodePacked(block.difficulty, block.timestamp));
   }
 
-  // deploys BoxV1 with a proxy
-  function deployBox(address boxTemplate) public returns(address) {
+  // deploys BoxV1 
+  function deployBasic() public returns(address) {
+    Box newBox = new Box();
+    
+    // emit an event 
+    address newBoxAddress = address(newBox);
+    emit BoxCreated(newBoxAddress);
+    return newBoxAddress;
+  }
+
+  // deploys BoxV1 with a minimal Clone
+  function deployClone(address boxTemplate) public returns(address) {
     
     // generate salt
-    bytes32 salt = _getSalt();  
+    bytes32 salt = _getSalt();
 
     // create new contract
     address newBox = address(uint160(boxTemplate.createClone2(salt)));
 
     // init contract
     // newBox.initiliaze(43);
+
+    // emit an event 
+    emit BoxCreated(newBox);
+
+    return newBox;
+
+  }
+
+  // deploys BoxV1 with a proxy
+  function deployWithProxy() public returns(address) {
+    
+    // deploy ProxyAdmin
+    ProxyAdmin proxyAdmin = new ProxyAdmin();
+
+    // deploy basic
+    address boxV1 = deployBasic();
+
+    // create a proxy
+    TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(boxV1, address(proxyAdmin), abi.encodePacked(uint16(0)));
+
+    // create new contract
+    address newBox = address(proxy);
 
     // emit an event 
     emit BoxCreated(newBox);
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,31 +1,57 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Deployer", function () {
-  it("Should deploy a Box contract properly", async function () {
-    
-    // deploy template
-    const Box = await ethers.getContractFactory("Box");
-    const box = await Box.deploy();
-    await box.deployed();
-    
-    // deploy 
-    const Deployer = await ethers.getContractFactory("Deployer");
-    const deployer = await Deployer.deploy();
-    await deployer.deployed();
 
-    // deploy an instance of box 
-    const tx = await deployer.deployBox(box.address)
+describe("Deployer", function () {
+  let deployer 
+  let Deployer 
+  let box 
+  let Box 
+  
+  const boxWorks = async (tx) => {
     const { events } = await tx.wait()
 
-    //
+    // check if box instance exists
     const evt = events.find((v) => v.event === 'BoxCreated')
     const { newBoxAddress } = evt.args
 
     // check if box instance works
     const newBox = Box.attach(newBoxAddress)
     await newBox.store(12)
-  
     expect(await newBox.retrieve()).to.equal(12);
-  });
+  }
+
+  beforeEach( async() => {
+    // deploy 
+    Deployer = await ethers.getContractFactory("Deployer");
+    deployer = await Deployer.deploy();
+    await deployer.deployed();
+    
+    // deploy template
+    Box = await ethers.getContractFactory("Box");
+    box = await Box.deploy();
+    await box.deployed();
+  })
+  describe("deployBasic", function () {
+    it("Should deploy a Box contract properly", async function () {
+      const tx = await deployer.deployBasic()
+      await boxWorks(tx)
+    });
+  })
+  describe("deployClone", function () {
+    it("Should deploy a Box contract properly", async function () {
+      // deploy using minimal clone 
+      const tx = await deployer.deployClone(box.address)
+      await boxWorks(tx)
+    });
+  })
+  describe("deployWithProxy", function () {
+    it("Should deploy a Box contract properly", async function () {
+      // deploy using proxy 
+      const tx = await deployer.deployWithProxy()
+      
+  
+      await boxWorks(tx)
+    });
+  })
 });
