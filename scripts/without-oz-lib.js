8a7ded199d3f70f74c7dfe80c17cb149ecca3e13
Clement Renaud <clement@clementrenaud.com>
2022-08-29 18:31:20 +0200

    improve simple proxy script

--- scripts/without-oz-lib.js
+++ scripts/without-oz-lib.js
@@ -3,44 +3,41 @@ const { ethers } = require('hardhat');
 
 async function main() {
 
-    const [, creator] = await ethers.getSigners()
+  const [, creator] = await ethers.getSigners()
 
-  // impl
+  // deploy impl v1
   const Box = await ethers.getContractFactory('Box');
   const box = await Box.deploy()
   await box.deployed()
-  console.log(box.address)
+  console.log('boxV1 impl:', box.address)
 
+  // encode initializer data
   const args = [creator.address]
   const fragment = box.interface.getFunction('initialize')
-  console.log(fragment)
   const data = box.interface.encodeFunctionData(fragment, args);
-  console.log(data)
-
-  // impl
+  
+  // deploy and init proxy
   const Proxy = await ethers.getContractFactory('SimpleProxy')
-  console.log(Proxy.interface)
   const proxy = await Proxy.deploy(box.address, data)
   await proxy.deployed()
+  console.log('implementation:', await proxy.implementation())
 
-  console.log(await proxy.implementation())
-
+  // check box v1
   const proxiedBox = await ethers.getContractAt('Box', proxy.address) 
   console.log(await proxiedBox.version())
 
-  // make an upgrade
+  // deploy v2 impl
   const BoxV2 = await ethers.getContractFactory('BoxV2');
   const boxV2 = await BoxV2.deploy()
   await boxV2.deployed()
   console.log(boxV2.address)
 
-  // upgrade
+  // upgrade to V2
   proxy.upgradeTo(boxV2.address)
 
-  //
+  // check box v2
   console.log(await proxiedBox.version())
 
-  
 }
 
 main();
